f43e3e304defa600da672f436e75bc7c4db21b56
John-wallace89 <john.wallace@heineken.co.uk>
2021-11-26 18:53:00 +0000

    Update - added product tiles to product page.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+# Views
+def products(request):
+    """ A view to show products in the barbell shop """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
