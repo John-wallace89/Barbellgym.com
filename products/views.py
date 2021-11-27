be4dd9dc4a8fdc387406313ea7914a9783d9437c
John-wallace89 <john.wallace@heineken.co.uk>
2021-11-27 14:20:24 +0000

    Update - added search functionality and fixed pep8 errors.

--- products/views.py
+++ products/views.py
@@ -1,17 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
-# Views
+# views
+
+
 def products(request):
-    """ A view to show products in the barbell shop """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    search = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            search = request.GET['q']
+            if not search:
+                messages.error(
+                    request, "Please enter search criteria to continue")
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=search) | Q(description__icontains=search)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': search,
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
