126df010830b9e1a8de2ad77a77bb24cc55cb53d
John-wallace89 <john.wallace@heineken.co.uk>
2021-11-28 15:27:11 +0000

    Update add context for basket app.

---
+++ basket/contexts.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+
+
+def basket_contents(request):
+
+    basket_items = []
+    total = 0
+    product_count = 0
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
