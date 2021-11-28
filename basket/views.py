5a1b2a31adc444f358084f771ed04816b9ac87cf
John-wallace89 <john.wallace@heineken.co.uk>
2021-11-28 16:04:39 +0000

    Update - added add to basket view.

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # views
+
 
 def view_basket(request):
     """ A view to return basket page """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of products to the basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
