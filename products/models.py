49728cca2e280123adece109b085a6fafd5335ca
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-03 16:52:50 +0000

    Fix - moved checkout models to correct location.

--- products/models.py
+++ products/models.py
@@ -28,6 +28,8 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    in_stock = models.BooleanField(default=True)
+
 
     def __str__(self):
         return self.name
