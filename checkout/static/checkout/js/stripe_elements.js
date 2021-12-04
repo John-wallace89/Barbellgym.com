7e35c6b6090dcff80808ccf29142c633513cb2fd
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-04 18:20:40 +0000

    Update - added checkout functionality with Stripe.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,5 +1,3 @@
-// js for stripe elements
-
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
@@ -7,9 +5,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-let client_secret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 let style = {
     base: {
@@ -22,10 +20,60 @@ let style = {
         }
     },
     invalid: {
-        // to match bootstrap danger class
+        // to match bootstrap alert class
         color: '#dc3545',
         iconColor: '#dc3545'
     }
 };
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submission
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    // Prevent multiple submissions after event has been called
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // Show error message to customer
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // Re-enable card to allow error to be fixed
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // Show success message to customer
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 
 # views
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    pay_intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K3095G8y5o5wiaYtBkPxNXkILjsDRDBMM87IhyEjZToMyhGwkhaJnNIdduQ1Ma2P86fHUqUpBk3qnwVrxitxnvP00g6K7Ymdn',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': pay_intent.client_secret,
     }
 
     return render(request, template, context)
