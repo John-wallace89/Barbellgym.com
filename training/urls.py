cd1d849b42d85d929d22169e565921e4a32861a0
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-22 16:53:22 +0000

    Update - added barbell class/pt page.

--- training/urls.py
+++ training/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.classes, name='classes'),
-    path('', views.personal_training, name='personal_trainers'),
+    path('', views.classes, name='barbell_classes'),
+    path('personal_training/', views.personal_training, name='personal_training'),
     # path('add/', views.add_product, name='add_product'),
     # path('edit/<int:product_id>', views.edit_product, name='edit_product'),
     # path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
--- training/views.py
+++ training/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Classes, PersonalTrainers
+
 
 # views
 def classes(request):
