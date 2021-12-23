924ce9d86d1315c6faa52cf4103176e3f2cb3f91
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-23 18:44:44 +0000

    Update - barbell classes view updated.

--- training/views.py
+++ training/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Classes, PersonalTrainers
 
@@ -7,7 +12,31 @@ from .models import Classes, PersonalTrainers
 def classes(request):
     """ A view to return Barbell Classes """
 
-    return render(request, 'training/barbell_classes.html')
+    classes = Classes.objects.all()
+    search = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+        if 'q' in request.GET:
+            search = request.GET['q']
+            if not search:
+                messages.error(
+                    request, "Please enter search criteria to continue")
+                return redirect(reverse('barbell_classes'))
+
+            queries = Q(
+                name__icontains=search) | Q(description__icontains=search)
+            products = products.filter(queries)
+
+    context = {
+        'classes': classes,
+        'search_term': search,
+    }
+
+    return render(request, 'training/barbell_classes.html', context)
 
 
 def personal_training(request):
