1cb80bd0b5f2bea288653c479f61798efdfa7880
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-24 11:28:11 +0000

    Fix - removed secret key.

--- barbellgym/settings.py
+++ barbellgym/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_a@l@#8&z%n-9yz)hp%&8v7x5l7b*4)f&k*f-*5pc(d#&u2&1k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'thebarbellgym.herokuapp.com',
