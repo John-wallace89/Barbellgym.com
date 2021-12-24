dbe26d32a335fd241e8c5eacae2c75b87ba22170
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-24 11:17:14 +0000

    Update - set up site for deployment.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn barbellgym.wsgi.application:application
--- barbellgym/settings.py
+++ barbellgym/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-_a@l@#8&z%n-9yz)hp%&8v7x5l7b*4)f&k*f-*5pc(d#&u2&1k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'thebarbellgym.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -122,12 +125,17 @@ WSGI_APPLICATION = 'barbellgym.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
