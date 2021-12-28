13dfcf557042b4b44b34113fb7940a05aac404a8
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-28 18:29:13 +0000

    Fix - indentation issue with settings

--- barbellgym/settings.py
+++ barbellgym/settings.py
@@ -148,8 +148,7 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': (
-            'django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator'),
+            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'),
     },
     {
         'NAME': (
