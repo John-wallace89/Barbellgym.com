581fd557c4271eb70d722581ad821917dc5da6f9
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-29 14:18:35 +0000

    Update - added add/edit class functionality.

--- training/urls.py
+++ training/urls.py
@@ -5,17 +5,7 @@ from . import views
 
 urlpatterns = [
     path('barbell_classes/', views.classes, name='barbell_classes'),
-    path('personal_training/',
-         views.personal_training,
-         name='personal_training'),
+    path('personal_training/', views.personal_training, name='personal_training'),
     path('add_class/', views.add_class, name='add_class'),
-    # path('add_pt/', views.add_pt, name='add_pt'),
-    # path('edit_class/<int:classes_id>', views.edit_class, name='edit_class'),
-    # path('edit/<int:personaltrainer_id>', views.edit_pt, name='edit_pt'),
-    # path('delete/<int:classes_id>/',
-    #       views.delete_class,
-    #       name='delete_class'),
-    # path('delete/<int:product_id>/',
-    #       views.delete_pt,
-    #       name='delete_pt'),
+    path('edit_class/<int:classes_id>/', views.edit_class, name='edit_class'),
 ]
--- training/views.py
+++ training/views.py
@@ -1,8 +1,11 @@
 """ Views for training app """
 
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Classes, PersonalTrainers
 from .forms import ClassesForm
@@ -51,8 +54,7 @@ def add_class(request):
             return redirect(reverse('barbell_classes'))
         else:
             messages.error(
-                request, 'Failed to add Class, \
-                please ensure form is valid')
+                request, 'Failed to add class, please ensure form is valid')
     else:
         form = ClassesForm()
 
@@ -62,3 +64,49 @@ def add_class(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_class(request, classes_id):
+    """ Edit a class in the store """
+    if not request.user.is_superuser:
+        messages.error(
+            request, 'Sorry, you do not have permisson to access this page.')
+        return redirect(reverse('home'))
+
+    classes = get_object_or_404(Classes, pk=classes_id)
+    if request.method == 'POST':
+        form = ClassesForm(request.POST, request.FILES, instance=classes)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated class!')
+            return redirect(reverse('barbell_classes'))
+        else:
+            messages.error(
+                request, 'Failed to update class. \
+                Please ensure the form is valid.')
+    else:
+        form = ClassesForm(instance=classes)
+        messages.info(request, f'You are editing {classes.name}')
+
+    template = 'training/edit_class.html'
+    context = {
+        'form': form,
+        'classes': classes,
+    }
+
+    return render(request, template, context)
+
+
+# @login_required
+# def delete_product(request, product_id):
+#     """ A view to delete a product """
+#     if not request.user.is_superuser:
+#         messages.error(request, 'Sorry, you do not have \
+#         permisson to access this page.')
+#         return redirect(reverse('home'))
+
+#     product = get_object_or_404(Product, pk=product_id)
+#     product.delete()
+#     messages.success(request, 'Product successfully deleted!')
+#     return redirect(reverse('products'))
