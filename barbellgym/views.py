50bb3000f6a719557ff0a5a61fac7a1bf5d7d9ad
John-wallace89 <john.wallace@heineken.co.uk>
2021-12-29 21:23:50 +0000

    Fix - updated view for custom 404 page.

--- barbellgym/views.py
+++ barbellgym/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 
 
 def error_404(request, exception):
-    data = {}
-    return render(request, 'barbellgym/404.html', data)
+    return render(request, '404.html')
 
 
 def error_500(request,  exception):
